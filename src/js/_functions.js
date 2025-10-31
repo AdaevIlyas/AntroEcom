03d33a29485d396a703f038336f1282d279fcffd
Ilyas <il_adaev@mail.ru>
2025-11-01 01:33:19 +0600

    finish

--- src/js/_functions.js
+++ src/js/_functions.js
@@ -19,8 +19,46 @@
 // });
 
 // Подключение плавной прокрутки к якорям
-// import SmoothScroll from 'smooth-scroll';
-// const scroll = new SmoothScroll('a[href*="#"]');
+// import SmoothScroll from "smooth-scroll";
+// const scroll = new SmoothScroll('a[href*="#"]', {
+//   position: "center"
+// });
+
+let links = document.querySelectorAll('a[href*="#"]');
+
+links.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    try {
+      e.preventDefault();
+
+      const href = link.hash;
+
+      const block = document.querySelector(`${href}`);
+
+      if (link.classList.contains("mobile-menu__link")) {
+        const mobileMenu = document.querySelector(".js-mobile-menu");
+        const burger = document.querySelector(".js-burger");
+        const menuBlack = document.querySelector(".mobile-menu__black");
+        mobileMenu.classList.remove("mobile-menu__slide_open");
+
+        document
+          .querySelector(".mobile-menu__black")
+          ?.classList.remove("mobile-menu__black_show");
+        burger.classList.remove("mobile-menu__burger_open");
+        burger.querySelector(".mobile-menu__burger-text").innerText = "Меню";
+        enable_scroll();
+
+        block.scrollIntoView({
+          block: "center",
+        });
+      } else {
+        block.scrollIntoView({
+          block: "center",
+        });
+      }
+    } catch (error) {}
+  });
+});
 
 // функция блокировки и разблокировки скролла
 // import { disable_scroll, enable_scroll } from "./functions/scroll";
@@ -29,6 +67,7 @@
 
 // добавление маски на телефон
 import phone_mask from "./functions/phone_mask";
+import { enable_scroll } from "./functions/scroll";
 phone_mask();
 
 // функция для добавления ограничения символов
--- src/js/components/feedbackFormAjax.js
+++ src/js/components/feedbackFormAjax.js
@@ -1,150 +1,184 @@
 export const feedbackFormAjax = () => {
-    const applicationForms = document.querySelectorAll('.js-form');
-    if (applicationForms.length) {
-        applicationForms.forEach(form => {
-            const inputs = form.querySelectorAll('input, textarea, select');
+  const applicationForms = document.querySelectorAll(".js-form");
+  if (applicationForms.length) {
+    applicationForms.forEach((form) => {
+      const inputs = form.querySelectorAll("input, textarea, select");
 
-            inputs.forEach(input => {
-                input.addEventListener('input', e => {
-                    validateInput(input);
-                });
-            });
+      inputs.forEach((input) => {
+        input.addEventListener("input", (e) => {
+          validateInput(input);
+        });
+      });
 
-            form.addEventListener('submit', e => {
-                e.preventDefault();
-                let valid = validateForm(form);
-                if (valid) {
-                    let formData = new FormData(form)
-                    formData.append('action', 'send_email');
-                    fetch('https://antro.cx/wp/wp-admin/admin-ajax.php', {
-                        method: 'POST',
-                        // mode: 'cors',
-                        body: formData
-                    }).then(response => response.json()).then(response => {
-                        if (response.success) {
-                            form.reset();
-                            if (form.classList.contains('main-order__form')) {
-                                form.reset();
-                                form.style.display = 'none';
-                                document.querySelector('.main-order__success').style.display = 'flex';
-                            } else if (form.classList.contains('audit-form__form')) {
-                                form.reset();
-                                document.querySelector(".js-modal-open[data-modal='thanks']").click();
-                            } else if (form.name == 'reviewForm') {
+      form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        let valid = validateForm(form);
+        if (valid) {
+          let formData = new FormData(form);
+          formData.append("action", "send_email");
+          fetch("https://antro.cx/wp/wp-admin/admin-ajax.php", {
+            method: "POST",
+            // mode: 'cors',
+            body: formData,
+          })
+            .then((response) => response.json())
+            .then((response) => {
+              if (response.success) {
+                form.reset();
+                if (form.classList.contains("main-order__form")) {
+                  form.reset();
+                  form.style.display = "none";
+                  document.querySelector(".main-order__success").style.display =
+                    "flex";
+                } else if (form.classList.contains("audit-form__form")) {
+                  form.reset();
+                  document
+                    .querySelector(".js-modal-open[data-modal='thanks']")
+                    .click();
+                } else if (form.name == "reviewForm") {
+                  // document.querySelector(".js-modal-open[data-modal='thanks']").click();
 
-                                // document.querySelector(".js-modal-open[data-modal='thanks']").click();
+                  const searchParams = new URLSearchParams();
+                  // Добавляем нужные параметры
+                  searchParams.append("type", "pay");
+                  if (formData.has("application-userEmail"))
+                    searchParams.append(
+                      "email",
+                      formData.get("application-userEmail")
+                    );
+                  if (formData.has("application-userTel"))
+                    searchParams.append(
+                      "phone",
+                      formData.get("application-userTel")
+                    );
+                  if (formData.has("application-userComMethod"))
+                    searchParams.append(
+                      "send_to",
+                      formData.get("application-userComMethod")
+                    );
 
-                                const searchParams = new URLSearchParams();
-                                // Добавляем нужные параметры
-                                searchParams.append('type', 'pay');
-                                if (formData.has('application-userEmail')) searchParams.append('email', formData.get('application-userEmail'));
-                                if (formData.has('application-userTel')) searchParams.append('phone', formData.get('application-userTel'));
-                                if (formData.has('application-userComMethod')) searchParams.append('send_to', formData.get('application-userComMethod'));
+                  // Формируем URL
+                  const baseUrl = response.data.report_url;
+                  const resultUrl = `${baseUrl}?${searchParams.toString()}`;
 
-                                // Формируем URL
-                                const baseUrl = response.data.report_url;
-                                const resultUrl = `${baseUrl}?${searchParams.toString()}`;
+                  window.location = resultUrl;
+                } else if (form.classList.contains("modal__inner")) {
+                  form.reset();
+                  form
+                    .closest('[data-modal="order"]')
+                    ?.classList.remove("modal_open");
 
-                                window.location = resultUrl;
-                            } else if (form.classList.contains('modal__inner')) {
-                                form.reset();
-                                form.closest('[data-modal="order"]')?.classList.remove('modal_open');
-
-                                document.querySelector('[data-modal="thanks"]')?.classList.add('modal_open');
-                            }
-                            ym(89555039, 'reachGoal', 'done');
-                        }
-                    });
+                  document
+                    .querySelector('[data-modal="thanks"]')
+                    ?.classList.add("modal_open");
                 }
-            })
-        })
-    }
+                ym(89555039, "reachGoal", "done");
+              }
+
+              const btn = form.querySelector(".form__send span");
+              const originalBtnText = btn.textContent;
+
+              btn.textContent = "Письмо отправлено!";
+              form.classList.add("disabled");
+
+              setTimeout(() => {
+                btn.textContent = "Отправить";
+                form.classList.remove("disabled");
+                form.reset();
+              }, 3000);
+            });
+        }
+      });
+    });
+  }
 };
 
 function validateInput(input) {
-    let isError = false;
-    let errorMessage = '';
-    const value = input.value.trim();
+  let isError = false;
+  let errorMessage = "";
+  const value = input.value.trim();
 
-    // Проверка обязательных полей
-    if (input.hasAttribute('data-required') && !value) {
-        isError = true;
-        errorMessage = input.getAttribute('data-required')
-        if (!errorMessage) {
-            errorMessage = 'Это поле обязательно для заполнения';
-        }
-        if (input.hasAttribute('data-email')) {
-            errorMessage = 'Пожалуйста, введите email';
-        }
+  // Проверка обязательных полей
+  if (input.hasAttribute("data-required") && !value) {
+    isError = true;
+    errorMessage = input.getAttribute("data-required");
+    if (!errorMessage) {
+      errorMessage = "Это поле обязательно для заполнения";
     }
-
-    // Проверка email-полей (только если поле заполнено)
-    if (!isError && input.hasAttribute('data-email') && value) {
-        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        if (!emailRegex.test(value)) {
-            isError = true;
-            errorMessage = 'Пожалуйста, проверьте правильность email’а';
-        }
+    if (input.hasAttribute("data-email")) {
+      errorMessage = "Пожалуйста, введите email";
     }
+  }
 
-    if (!isError && input.type == 'checkbox' && !input.checked) {
-        console.log('input')
-        isError = true;
-        let parent = input.closest('.i-checkbox');
-
-        if (!parent) return;
-
-        parent.classList.add('i-checkbox_error');
+  // Проверка email-полей (только если поле заполнено)
+  if (!isError && input.hasAttribute("data-email") && value) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(value)) {
+      isError = true;
+      errorMessage = "Пожалуйста, проверьте правильность email’а";
     }
+  }
 
-    // Поиск родительского контейнера
-    let errorElement = null;
-    const inputBlock = input.closest('.form__field');
+  if (!isError && input.type == "checkbox" && !input.checked) {
+    console.log("input");
+    isError = true;
+    let parent = input.closest(".i-checkbox");
+
+    if (!parent) return;
+
+    parent.classList.add("i-checkbox_error");
+  }
+
+  // Поиск родительского контейнера
+  let errorElement = null;
+  const inputBlock = input.closest(".form__field");
+  if (inputBlock) {
+    // Работа с сообщением об ошибке
+    errorElement = inputBlock.querySelector(".form__field-error-text");
+  }
+
+  if (isError) {
+    console.log(isError, "isError");
+
     if (inputBlock) {
-        // Работа с сообщением об ошибке
-        errorElement = inputBlock.querySelector('.form__field-error-text');
+      inputBlock.classList.add("error");
     }
 
-    if (isError) {
-        console.log(isError, 'isError');
-
-        if (inputBlock) {
-            inputBlock.classList.add('error');
-        }
-
-        // Создаем элемент ошибки если отсутствует
-        if (!errorElement) {
-            errorElement = document.createElement('div');
-            errorElement.className = 'form__field__error';
-            if (inputBlock) {
-                inputBlock.appendChild(errorElement);
-            }
-        }
-        errorElement.textContent = errorMessage;
-        return false;
+    // Создаем элемент ошибки если отсутствует
+    if (!errorElement) {
+      errorElement = document.createElement("div");
+      errorElement.className = "form__field__error";
+      if (inputBlock) {
+        inputBlock.appendChild(errorElement);
+      }
     }
-    if (inputBlock && errorElement) {
-        inputBlock.classList.remove('error');
-        errorElement.textContent = '';
-    }
+    errorElement.textContent = errorMessage;
+    return false;
+  }
+  if (inputBlock && errorElement) {
+    inputBlock.classList.remove("error");
+    errorElement.textContent = "";
+  }
 
-    return true;
+  return true;
 }
 
 function validateForm(form) {
-    let formIsValid = true;
+  let formIsValid = true;
 
-    const inputs = form.querySelectorAll('input:not([type="hidden"]), textarea, select');
+  const inputs = form.querySelectorAll(
+    'input:not([type="hidden"]), textarea, select'
+  );
 
-    inputs.forEach(input => {
-        let isValidInput = validateInput(input);
-        console.log(isValidInput, 'isValidInput');
+  inputs.forEach((input) => {
+    let isValidInput = validateInput(input);
+    console.log(isValidInput, "isValidInput");
 
-        if (!isValidInput) {
-            formIsValid = false;
-        }
-    });
-    console.log(formIsValid, 'formIsValid');
+    if (!isValidInput) {
+      formIsValid = false;
+    }
+  });
+  console.log(formIsValid, "formIsValid");
 
-    return formIsValid;
+  return formIsValid;
 }
